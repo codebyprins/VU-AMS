cc8459629546228d9f17cbf1cd46c6268576ccc4
Roy-Aa <34898@ma-web.nl>
2026-04-09 12:56:56 +0200

    ACF Flexible blocks setup

---
+++ resources/views/components/flexible_blocks.php
@@ -0,0 +1,38 @@
+<?php if (have_rows('flexible_blocks')): ?>
+    <?php while (have_rows('flexible_blocks')): the_row(); ?>
+
+        <?php
+        $layout = get_row_layout();
+
+        if ($layout == 'banner') :
+            get_template_part('resources/views/sections/banner');
+
+        elseif ($layout == 'slider') :
+            get_template_part('resources/views/sections/slider');
+
+        elseif ($layout == 'map') :
+            get_template_part('resources/views/sections/map');
+
+        elseif ($layout == 'testimonials') :
+            get_template_part('resources/views/sections/testimonials');
+
+        elseif ($layout == 'faq') :
+            get_template_part('resources/views/sections/faq');
+
+        elseif ($layout == 'cta') :
+            get_template_part('resources/views/sections/cta');
+
+        elseif ($layout == 'stats') :
+            get_template_part('resources/views/sections/stats');
+
+        elseif ($layout == 'text_block') :
+            get_template_part('resources/views/sections/text_block');
+
+        elseif ($layout == 'history') :
+            get_template_part('resources/views/sections/history');
+
+        endif;
+        ?>
+
+    <?php endwhile; ?>
+<?php endif; ?>
