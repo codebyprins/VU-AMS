2e7ba84e9a42cca48ed499829b77d8941ba8808b
Astrid35829 <astridprins2@gmail.com>
2026-04-09 12:02:25 +0200

    added color, btn and padding defaults

--- resources/views/sections/vb-section.php
+++ resources/views/sections/vb-section.php
@@ -1,12 +1,14 @@
 <section class="section">
     <div class="container py-8">
         <div class="grid grid-cols-2 gap-4 items-center">
-            <div class="col-span-1">
+            <div class="col-span-1 flex flex-col gap-4">
                 <h2 class="text-green-200 text-xl">Virtual Box</h2>
                 <p class="text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, sed efficitur ligula. Donec a nunc eget nisl convallis commodo. Sed at felis ac enim efficitur efficitur. Donec a nunc eget nisl convallis commodo.</p>
+                <a href="" class="btn btn-primary">Learn More</a>
+                <a href="" class="btn btn-primary-outline">Get Started</a>
             </div>
             <div class="col-span-1 p-4">
-                <div class="bg-gray w-full h-full"></div>
+                <div class="bg-gray-200 w-full h-full"></div>
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,25 @@ module.exports = {
     extend: {
       colors: {
         primary: '#00B6CB',
-        'primary-light': '#ABE0E6',
+        primary_light: '#ABE0E6',
+        primary_dark: '#018898',
         secondary: '#F7C80C',
-        'secondary-light': '#EDE1B4',
+        secondary_light: '#EDE1B4',
         accent: '#101935',
       },
       fontFamily: {
         sans: ['Heebo', 'sans-serif'],
+      },
+      padding: {
+        section_base: '80px',
+        section_medium: '40px',
+        section_small: '20px',
+        container_base: '60px',
+        container_small: '40px',
+        container_large: '80px',
+      },
+      borderRadius: {
+        'base': '12px',
       },
     },
   },
