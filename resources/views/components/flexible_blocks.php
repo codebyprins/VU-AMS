cf9dbdb2946da796c1947b7d22b9d411f849d473
Roy-Aa <34898@ma-web.nl>
2026-04-13 15:01:59 +0200

    Not done - publications functionality and styling

--- resources/views/components/flexible_blocks.php
+++ resources/views/components/flexible_blocks.php
@@ -31,6 +31,9 @@
         elseif ($layout == 'history') :
             get_template_part('resources/views/sections/history');
 
+        elseif ($layout == 'publications_overview') :
+            get_template_part('resources/views/sections/publications_overview');
+
         endif;
         ?>
 
