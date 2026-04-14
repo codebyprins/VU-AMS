604f4de712f0726bad3f81afb0fd888e4e24fe1e
ying y <cookie15102@gmail.com>
2026-04-14 10:26:38 +0200

    Added flexible block

--- resources/views/components/flexible_blocks.php
+++ resources/views/components/flexible_blocks.php
@@ -40,6 +40,9 @@
         elseif ($layout == 'product_cards') :
             get_template_part('resources/views/sections/product_cards');
 
+        elseif ($layout == 'productUSBS') :
+            get_template_part('resources/views/sections/productUSBS');
+
         endif;
         ?>
 
