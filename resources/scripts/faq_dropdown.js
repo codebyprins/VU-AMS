4910f7ea531b8d192f8e2bc3a2aea51e63d8fd65
Roy-Aa <royaardenburg@gmail.com>
2026-04-20 12:36:52 +0200

    Revert "- Added a max width + a close old faq item function."
    This reverts commit 96b3892c206fbe4dc369d4709fe5508de8c071ce.

--- resources/scripts/faq_dropdown.js
+++ resources/scripts/faq_dropdown.js
@@ -9,7 +9,7 @@ const initFaqAccordion = () => {
             return;
         }
 
-        header.style.cursor = 'pointer';
+            header.style.cursor = 'pointer';
         header.setAttribute('role', 'button');
         header.setAttribute('tabindex', '0');
         header.setAttribute('aria-expanded', 'false');
@@ -39,27 +39,6 @@ const initFaqAccordion = () => {
         };
 
         const openFaq = () => {
-            faqItems.forEach((otherItem) => {
-                if (otherItem !== item) {
-                    const otherContent = otherItem.querySelector('.faq_content');
-                    const otherHeader = otherItem.querySelector('.faq_header');
-                    const otherPlus = otherItem.querySelector('.faq_icon--plus');
-                    const otherMinus = otherItem.querySelector('.faq_icon--minus');
-
-                    if (otherContent && otherHeader) {
-                        otherContent.style.maxHeight = '0';
-                        otherContent.style.opacity = '0';
-                        otherHeader.setAttribute('aria-expanded', 'false');
-                        otherItem.classList.remove('faq_open');
-
-                        if (otherPlus && otherMinus) {
-                            otherPlus.classList.remove('hidden');
-                            otherMinus.classList.add('hidden');
-                        }
-                    }
-                }
-            });
-
             const fullHeight = content.scrollHeight;
             content.style.maxHeight = `${fullHeight}px`;
             content.style.opacity = '1';
