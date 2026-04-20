96b3892c206fbe4dc369d4709fe5508de8c071ce
Roy-Aa <royaardenburg@gmail.com>
2026-04-20 12:35:27 +0200

    - Added a max width + a close old faq item function.

--- resources/views/sections/faq.php
+++ resources/views/sections/faq.php
@@ -5,10 +5,10 @@ $faq_items = get_sub_field('faq_items');
 ?>
 
 <section class="bg-white py-10">
-    <div class="container mx-auto px-4">
+    <div class="container mx-auto px-4 flex justify-center items-center">
         <div class="faq_items">
             <?php foreach ($faq_items as $faq_item) : ?>
-                <div class="faq_item mb-5 border-b border-black">
+                <div class="faq_item mb-5 border-b border-black max-w-[600px]">
                     <div class="faq_header flex items-center justify-between">
                         <h3 class="text-2xl mb-4"><?php echo $faq_item['question']; ?></h3>
 
