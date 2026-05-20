2271fb8a4d1a98489a71bf9090239ddec09a7c81
Roy-Aa <34898@ma-web.nl>
2026-05-20 10:31:04 +0200

    - Icon fix

--- resources/views/sections/faq.php
+++ resources/views/sections/faq.php
@@ -10,7 +10,7 @@ $faq_items = get_sub_field('faq_items');
             <?php foreach ($faq_items as $faq_item) : ?>
                 <div class="faq_item mb-5 border-b border-black">
                     <div class="faq_header flex items-center justify-between">
-                        <h3 class="text-2xl mb-4"><?php echo $faq_item['question']; ?></h3>
+                        <h3 class="text-2xl mb-4 max-w-[95%]"><?php echo $faq_item['question']; ?></h3>
 
                         <svg class="faq_icon faq_icon--plus mb-3" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12 4V20M20 12H4" stroke="#141B34" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
