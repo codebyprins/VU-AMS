dd8514b3f11021648fd0beeb30e4375c9a153e80
Astrid35829 <astridprins2@gmail.com>
2026-05-22 11:18:57 +0200

    file variable got removed in develop, broke flexible layout. readded for fix

--- resources/views/components/flexible_blocks.php
+++ resources/views/components/flexible_blocks.php
@@ -3,12 +3,13 @@
 
         <?php
         $layout = get_row_layout();
+        $file = "resources/views/sections/{$layout}.php";
 
-    if (locate_template($file)) {
-        get_template_part("resources/views/sections/{$layout}");
-    } else {
-        echo "<strong>MISSING TEMPLATE:</strong> {$file}";
-    }?>
+        if (locate_template($file)) {
+            get_template_part("resources/views/sections/{$layout}");
+        } else {
+            echo "<strong>MISSING TEMPLATE:</strong> {$file}";
+        } ?>
 
     <?php endwhile; ?>
 <?php endif; ?>
