45b655a4a58f1914f7750a73bbd44ec5482cd2e3
Roy-Aa <34898@ma-web.nl>
2026-05-26 11:27:10 +0200

    Single.php fix

--- single.php
+++ single.php
@@ -1,23 +1,5 @@
-<?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-?>
-
 <?php get_header(); ?>
 
-<?php
-// Override the field name for posts
-if (have_rows('post_blocks')):
-    while (have_rows('post_blocks')): the_row();
-        $layout = get_row_layout();
-        $file = "resources/views/sections/{$layout}.php";
-        if (locate_template($file)) {
-            get_template_part("resources/views/sections/{$layout}");
-        } else {
-            echo "<strong>MISSING TEMPLATE:</strong> {$file}";
-        }
-    endwhile;
-endif;
-?>
+<?php get_template_part('resources/views/components/flexible_blocks'); ?>
 
 <?php get_footer(); ?>
