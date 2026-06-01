4287f8c86e9bbe76ea74a332bf40d45494eabd17
Roy-Aa <34898@ma-web.nl>
2026-06-01 13:59:49 +0200

    - FAQ & Search optimize

--- resources/views/sections/faq.php
+++ resources/views/sections/faq.php
@@ -1,16 +1,23 @@
 <?php
 
-$faq_items = get_sub_field('faq_items');
+$title = get_sub_field('title');
+
+$faq_posts = get_posts([
+    'post_type' => 'faq',
+    'posts_per_page' => -1,
+    'post_status' => 'publish'
+]);
 
 ?>
 
 <section class="bg-white py-10">
-    <div class="container mx-auto px-4 flex justify-center items-center">
+    <div id="faq" class="container mx-auto px-4 flex justify-center items-center">
         <div class="faq_items max-w-[600px]">
-            <?php foreach ($faq_items as $faq_item) : ?>
+            <h2 class="text-4xl font-bold mb-14 text-center"><?php echo esc_html($title); ?></h2>
+            <?php foreach ($faq_posts as $faq_post) : ?>
                 <div class="faq_item mb-5 border-b border-black">
                     <div class="faq_header flex items-center justify-between">
-                        <h3 class="text-2xl mb-4 max-w-[95%]"><?php echo $faq_item['question']; ?></h3>
+                        <h3 class="text-2xl mb-4 max-w-[95%]"><?php echo esc_html($faq_post->post_title); ?></h3>
 
                         <svg class="faq_icon faq_icon--plus mb-3" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12 4V20M20 12H4" stroke="#141B34" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -22,7 +29,7 @@ $faq_items = get_sub_field('faq_items');
 
                     </div>
                     <div class="faq_content">
-                        <div class="rich-editor px-4 pb-4 text-black"><?php echo $faq_item['answer']; ?></div>
+                        <div class="rich-editor px-4 pb-4 text-black"><?php echo get_field('answer', $faq_post->ID); ?></div>
                     </div>
                 </div>
             <?php endforeach; ?>
