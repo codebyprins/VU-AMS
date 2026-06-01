caf43a8adbce2a28210c331ecd62a8b41b08a1cd
Kjell06 <34952@ma-web.nl>
2026-06-01 13:45:11 +0200

    Add CF7 integration to dynamically populate select fields with product data

---
+++ functions/cf7.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Dynamically populate CF7 select fields with post-type data.
+ */
+add_filter('wpcf7_form_tag', function ($tag) {
+    if (!is_array($tag) || ($tag['name'] ?? '') !== 'your-product') {
+        return $tag;
+    }
+
+    $products = get_posts([
+        'post_type'      => 'product',
+        'posts_per_page' => -1,
+        'post_status'    => 'publish',
+        'orderby'        => 'title',
+        'order'          => 'ASC',
+    ]);
+
+    $options = array_map(fn($p) => $p->post_title, $products);
+
+    $tag['values'] = $options;
+    $tag['labels'] = $options;
+
+    return $tag;
+});
